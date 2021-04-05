0e01e8a60f12b10a69139a1b016b95049d1714dc
Jxq7 <79334626+Jxq7@users.noreply.github.com>
2021-04-05 02:14:14 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,8 @@
+# we can store stuff in our program using variables really ez ill show you now
+# so to create a variable we give it a name we set it = to a value like
+a = print("412")
+# in this case a string called "412" use either double quotes or single quotes  choice is yours
+# we can also store multiple strings  in a variable if we want using something called a list so i can create a variable called b and if i wanna store 2 strings this time example here
+b = ["Hello world","Francis"] # now we stored that in a list using 2 sqaure brackets to set a list and we tell  seperated with a comma to get an item
+# out of a list we need to know its position in the list if i said b[1] it'll be only francis left bc i just deleted hello world we can also add list together
+# ok im going to bed cya its 2:14 am
